e49371e09b38c089578ce9c0ed089d0a62eaf5f9
Maira Anabella <anabellamaira3@gmail.com>
2022-09-21 14:50:59 -0300

    borre css y js de reproductor , cambie de lugar logo y descripcion

---
+++ js/script.js
@@ -0,0 +1,165 @@
+//--------REPRODUCTOR--------
+// Song data
+const songList = [
+    {
+        title: "Hey Jude",
+        file: "hey jude.mp3",
+        cover: "1.jpg"
+
+    },
+    {
+        title: "Stay",
+        file: "Stay.mp3",
+        cover: "2.jpg"
+    },
+    {
+        title: "Unstoppable",
+        file: "Unstoppable.mp3",
+        cover: "3.jpg"
+    },
+]
+
+// Canción actual
+let actualSong = null
+
+// Capturar elementos del DOM para trabajar con JS
+const songs = document.getElementById("songs")
+const audio = document.getElementById("audio")
+const cover = document.getElementById("cover")
+const title = document.getElementById("title")
+const play = document.getElementById("play")
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+const progress = document.getElementById("progress")
+const progressContainer = document.getElementById("progress-container")
+progressContainer.addEventListener("click", setProgress)
+
+// Escuchar el elemento AUDIO
+audio.addEventListener("timeupdate", updateProgress)
+
+// Escuchar clicks en los controles
+play.addEventListener("click", () => {
+    if (audio.paused) {
+        playSong()   
+    } else {
+        pauseSong()
+    }
+})
+
+next.addEventListener("click", () => nextSong())
+prev.addEventListener("click", () => prevSong())
+
+// Cargar canciones y mostrar el listado
+function loadSongs() {
+    songList.forEach((song, index) => {
+        // Crear li
+        const li = document.createElement("li")
+        // Crear a
+        const link = document.createElement("a")
+        // Hidratar a
+        link.textContent = song.title
+        link.href = "#"
+        // Escuchar clicks
+        link.addEventListener("click", () => loadSong(index))
+        // Añadir a li
+        li.appendChild(link)
+        // Aañadir li a ul
+        songs.appendChild(li)
+    })
+}
+
+// Cargar canción seleccionada
+function loadSong(songIndex) {
+    if (songIndex !== actualSong) {
+        changeActiveClass(actualSong, songIndex)
+        actualSong = songIndex
+        audio.src = "../audio/" + songList[songIndex].file
+        playSong()
+        changeSongtitle(songIndex)
+        changeCover(songIndex)
+    }
+}
+
+// Actualizar barra de progreso de la canción
+function updateProgress(event) {
+    const {duration, currentTime} = event.srcElement
+    const percent = (currentTime / duration) * 100
+    progress.style.width = percent + "%" 
+}
+
+// Hacer la barra de progreso clicable
+function setProgress(event) {
+    const totalWidth = this.offsetWidth
+    const progressWidth = event.offsetX
+    const current = (progressWidth / totalWidth) * audio.duration
+    audio.currentTime = current
+}
+
+// Actualiar controles
+function updateControls() {
+    if (audio.paused) {
+        play.classList.remove("fa-pause")
+        play.classList.add("fa-play")
+    } else {
+        play.classList.add("fa-pause")
+        play.classList.remove("fa-play")
+    }
+}
+
+// Reproducir canción
+function playSong() {
+    if (actualSong !== null) {
+        audio.play()
+        updateControls()
+    }
+}
+
+// Pausar canción
+function pauseSong() {
+    audio.pause()
+    updateControls()
+}
+
+// Cambiar clase activa
+function changeActiveClass(lastIndex, newIndex) {
+    const links = document.querySelectorAll("a")
+    if (lastIndex !== null) {
+        links[lastIndex].classList.remove("active")
+    }
+    links[newIndex].classList.add("active")
+}
+
+cover.src= "../img/portada.png"
+// Cambiar el cover de la canción
+function changeCover(songIndex) {
+    cover.src = "../img/" + songList[songIndex].cover
+}
+
+// Cambiar el título de la canción
+function changeSongtitle(songIndex) {
+     title.innerText = songList[songIndex].title
+}
+
+// Anterior canción
+function prevSong() {
+    if (actualSong > 0) {
+        loadSong(actualSong - 1)
+    } else {
+        loadSong(songList.length - 1)
+    }
+}
+
+// Siguiente canción
+function nextSong() {
+    if (actualSong < songList.length -1) {
+        loadSong(actualSong + 1)
+    } else {
+        loadSong(0)
+    }
+}
+
+// Lanzar siguiente canción cuando se acaba la actual
+audio.addEventListener("ended", () => nextSong())
+
+// GO!
+loadSongs()
